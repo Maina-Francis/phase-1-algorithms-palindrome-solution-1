b5504f060f76f70e957b0cea73cedc1a20f02269
Francis Maina <sebastianfrancix@gmail.com>
2022-08-31 01:07:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let alphabets = word.split("");
+  let reversedAlphabets = alphabets.reverse();
+  let reverseString = reversedAlphabets.join("");
+
+  if (word === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
